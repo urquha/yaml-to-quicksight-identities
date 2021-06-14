ae2776c6ef781733cd8b6308ba3ea614475ca40b
urquha <andy.urquhart@hotmail.co.uk>
2021-06-14 11:56:06 +0100

    initial commit

--- quicksight_identity.tf
+++ quicksight_identity.tf
@@ -1,10 +1,10 @@
-resource "aws_quicksight_group" "quicksight_group" {
+resource "aws_quicksight_group" "quicksight_groups" {
   for_each = toset(yamldecode(file("quicksight_groups.yml"))["quicksight"])
   group_name = each.value
 }
 
 resource "aws_quicksight_user" "quicksight_users" {
-  for_each      = { for item in yamldecode(file("users.yml"))["user"] : item.email => item }
+  for_each      = { for item in yamldecode(file("quicksight_users.yml"))["user"] : item.email => item }
   user_name     = each.value.email
   email         = each.value.email
   identity_type = "IAM"
--- variables.tf
+++ variables.tf
@@ -2,7 +2,3 @@ variable "region" {
   default = "eu-west-2"
 }
 
-# variable "user_list" {
-#   type = list
-#   default = yamldecode(file("users.yml"))["users"]
-# }
